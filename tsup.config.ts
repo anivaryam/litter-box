0ae21ad01259b46afa50b9d8e66e84056664e2db
anivaryam <anivaryam.dev@gmail.com>
2026-06-16 01:58:39 +0800

    chore: scaffold litter-box project

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig([
+  {
+    entry: { index: "src/index.ts", react: "src/react.tsx" },
+    format: ["esm", "cjs"],
+    dts: true,
+    clean: true,
+    external: ["react", "react-dom"],
+  },
+  {
+    entry: { "litter-box.global": "src/index.ts" },
+    format: ["iife"],
+    globalName: "LitterBoxLib",
+    minify: true,
+  },
+]);
